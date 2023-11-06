ad086383c9575a78a618369e44a5c666cf1a0c13
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-11-06 22:11:03 +0100

    fix: customer creation fix and profile menu loading

--- src/components/navbar/AuthMenu.tsx
+++ src/components/navbar/AuthMenu.tsx
@@ -1,16 +1,17 @@
 'use client';
 
 import { DropdownMenu } from '@/components/dropdown-menu/DropdownMenu';
+import { Spinner } from '@/components/spinner/Spinner';
 import { TextLink } from '@/components/text-Link/TextLink';
-import { authenticatedApiService } from '@/services/authenticatedApi.service';
+import { useMe } from '@/hooks/customer/profile/useMe';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { isAxiosError } from 'axios';
 import { redirect } from 'next/navigation';
-import useSWR from 'swr';
 
 export function AuthMenu() {
   const { user } = useUser();
-  const { error } = useSWR('/customer/me', () => authenticatedApiService.customerControllerGetMe());
+  const { error, isLoading } = useMe();
+  if (isLoading) return <Spinner size='sm' />;
   if (!user)
     return (
       <TextLink className='text-lg' href='/api/auth/login'>
--- src/hooks/customer/profile/useMe.ts
+++ src/hooks/customer/profile/useMe.ts
@@ -2,8 +2,12 @@ import { authenticatedApiService } from '@/services/authenticatedApi.service';
 import useSWR from 'swr';
 
 export function useMe() {
-  return useSWR('useMe', async () => {
-    const response = await authenticatedApiService.customerControllerGetMe();
-    return response.data;
-  });
+  return useSWR(
+    'useMe',
+    async () => {
+      const response = await authenticatedApiService.customerControllerGetMe();
+      return response.data;
+    },
+    { revalidateOnFocus: false }
+  );
 }
